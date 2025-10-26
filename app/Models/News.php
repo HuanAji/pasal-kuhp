b8e31517ab73cec6a6cc309085e3871e8eb0fbfa
HuanAji <123148327+HuanAji@users.noreply.github.com>
2025-10-26 21:13:23 +0700

    Initial commit

---
+++ app/Models/News.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $fillable = [
+        'author_id',
+        'news_category_id',
+        'title',
+        'slug',
+        'thumbnail',
+        'content',
+    ];
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function nwsCategory()
+    {
+        return $this->belongsTo(NewsCategory::class, 'news_category_id');
+    }
+    
+    public function banner()
+    {
+        return $this->hasOne(Banner::class);
+    }
+}
