e8fb164bda4ca902fefc994c82de2fe64e5fae1e
HuanAji <123148327+HuanAji@users.noreply.github.com>
2025-10-29 23:43:18 +0700

    Fix typo in newsCategory method name in News model

--- app/Models/News.php
+++ app/Models/News.php
@@ -19,7 +19,7 @@ class News extends Model
         return $this->belongsTo(Author::class);
     }
 
-    public function nwsCategory()
+    public function newsCategory()
     {
         return $this->belongsTo(NewsCategory::class, 'news_category_id');
     }
