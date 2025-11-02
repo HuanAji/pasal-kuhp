2baac25aee2bf80cce4cd4c0d1d5664b563d5da7
HuanAji <123148327+HuanAji@users.noreply.github.com>
2025-11-03 02:28:30 +0700

    add banner, featured and news for landing page view

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -1,13 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Banner;
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
     public function index()
     {
-        return view('pages.landing');
+        $banners = Banner::all();
+        $featureds = News::where('is_featured', true)->get();
+        $news = News::orderBy('created_at', 'desc')->take(4)->get();
+
+        return view('pages.landing', compact('banners', 'featureds', 'news'));
     }
 }
