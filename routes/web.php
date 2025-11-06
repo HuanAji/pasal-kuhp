ded3ec55319a9e42352734168755cde18a0d6062
HuanAji <123148327+HuanAji@users.noreply.github.com>
2025-11-06 08:18:34 +0700

    add some routes for landing and news

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\LandingController;
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingController::class, 'index'])->name('landing');
+
+Route::get('/news/{slug}', [NewsController::class, 'show'])->name('news.show');
