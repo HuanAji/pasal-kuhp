9967c49f8a535a92099fc63e9fa29a56aee4e7ea
HuanAji <123148327+HuanAji@users.noreply.github.com>
2025-11-07 23:50:35 +0700

    adding route authow show author name

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
@@ -7,3 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [LandingController::class, 'index'])->name('landing');
 
 Route::get('/news/{slug}', [NewsController::class, 'show'])->name('news.show');
+
+Route::get('/author/{username}', [AuthorController::class, 'show'])->name('author.show');
