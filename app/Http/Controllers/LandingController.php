05c2b81351a33aec5bbfbcc6a237733ad070a2ec
HuanAji <123148327+HuanAji@users.noreply.github.com>
2025-12-14 12:28:16 +0700

    delete banner

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Author;
-use App\Models\Banner;
 use App\Models\News;
 use Illuminate\Http\Request;
 
@@ -11,10 +10,21 @@ class LandingController extends Controller
 {
     public function index()
     {
-        $banners = Banner::all();
-        $featureds = News::where('is_featured', true)->get();
-        $news = News::orderBy('created_at', 'desc')->take(4)->get();
-        $authors = Author::all()->take(5);
-        return view('pages.landing', compact('banners', 'featureds', 'news', 'authors'));
+        $featureds = News::with(['author', 'newsCategory'])
+            ->where('is_featured', true)
+            ->orderBy('created_at', 'desc')
+            ->take(4)
+            ->get();
+        
+        $news = News::with(['author', 'newsCategory'])
+            ->orderBy('created_at', 'desc')
+            ->take(4)
+            ->get();
+        
+        $authors = Author::withCount('news')
+            ->take(5)
+            ->get();
+        
+        return view('pages.landing', compact('featureds', 'news', 'authors'));
     }
 }
